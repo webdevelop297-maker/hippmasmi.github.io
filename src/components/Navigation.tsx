cb871a69d980ff44e1e509c1de8727b4fb2962d1
manhdpk <arfanmaulana@madrasah.kemenag.go.id>
2025-09-17 13:15:48 +0700

    update berita

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,7 +14,7 @@ const Navigation = () => {
     { path: "/struktur", label: "Struktur" },
     { path: "/sejarah", label: "Sejarah" },
     { path: "/program", label: "Program" },
-    { path: "/NewsAll", label: "Berita" },
+    { path: "/News", label: "Berita" },
     { path: "/database-kader", label: "Database Kader" },
   ];
 
